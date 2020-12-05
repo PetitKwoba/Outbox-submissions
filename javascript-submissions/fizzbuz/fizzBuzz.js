88d4fcef3a85ae4cd64d0966cb88008bb7a50c3c
Petit Emmanuel <kwobapetit@gmail.com>
2020-12-05 20:09:56 +0300

    added paragraph to fizzbuzz

--- javascript-submissions/fizzbuz/fizzBuzz.js
+++ javascript-submissions/fizzbuz/fizzBuzz.js
@@ -1,27 +1,30 @@
 function fizBuzz(string1, string2) {
     let value1 = document.getElementById("fizz").value;
     let value2 = document.getElementById("buzz").value;
+    let outPuts = document.getElementById("outPut");
 
     console.log(value1);
-    console.log(value2)
+    console.log(value2);
     value1Length = parseInt(value1.length);
 
     console.log("The length of first string is: " + value1Length);
     value2Length = parseInt(value2.length);
     console.log("The length of second string is: " + value2Length);
 
-
-    totalLegnth = (value1Length + value2Length)
+    totalLegnth = (value1Length + value2Length);
     console.log("totalLegnth: " + totalLegnth);
 
     if (totalLegnth % 3 === 0) {
         console.log("Fiz")
+        outPuts.innerHTML = ("Fiz")
     } else if (totalLegnth % 5 === 0) {
         console.log("Buz")
+        outPuts.innerHTML("Buzz")
     } else if (totalLegnth % 3 == 0 && totalLegnth % 5 == 0) {
         console.log("fizBuzz")
+        outPuts.innerHTML = ("FizzBuzz")
     } else {
-        console.log("None of above")
+        console.log("neither divisble by 3 or 9")
     }
     return (fizBuzz);
 
